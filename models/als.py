e9b78ad15ea376aa65a8b5507343c3ae98220c90
aayush <aayushyadav96@gmail.com>
2019-12-07 23:12:35 -0500

    modify bash

---
+++ models/als.py
@@ -0,0 +1,1 @@
+from pyspark.ml.recommendation import ALS
--- run.sh
+++ run.sh
@@ -6,6 +6,35 @@ BUCKET_NAME=<bucket-name>
 CLUSTER=<cluster-name>
 ZONE="us-west1-a"
 
+# create the working directories and chunk the data if not already chunked
+if ! [ -d gs://${BUCKET_NAME}/chunks/reviews ]; then
+    mkdir -p gs://${BUCKET_NAME}/chunks/reviews
+    split --bytes 500M --numeric-suffixes --suffix-length=3 \
+          --additional-suffix=.json gs://${BUCKET_NAME}/review_Books_5.json \
+          reviews_
+    mv gs://${BUCKET_NAME}/reviews_0*.json gs://${BUCKET_NAME}/chunks/reviews/
+    # rm review_Books_5.json
+fi
+
+if ! [ -d gs://${BUCKET_NAME}/chunks/metabooks ]; then
+    mkdir -p gs://${BUCKET_NAME}/chunks/metabooks
+    split --bytes 500M --numeric-suffixes --suffix-length=3 \
+          --additional-suffix=.json gs://${BUCKET_NAME}/metaBooks.json \
+          metabooks_
+    mv gs://${BUCKET_NAME}/metabooks_0*.json gs://${BUCKET_NAME}/chunks/metabooks/
+    # rm metaBooks.json
+fi
+
+# chunk the full dataset if it has been generated
+if [ -f gs://${BUCKET_NAME}/data.json ] && \
+ ! [ -d gs://${BUCKET_NAME}/chunks/processed ]; then
+    mkdir -p gs://${BUCKET_NAME}/chunks/processed
+    split --bytes 500M --numeric-suffixes --suffix-length=4 \
+          --additional-suffix=.json gs://${BUCKET_NAME}/processed.json \
+          processed_
+    mv gs://${BUCKET_NAME}/processed_0*.json gs://${BUCKET_NAME}/chunks/processed/
+    # rm -r chunks/metabooks chunks/reviews
+fi
 
 # create a cloud Dataproc cluster
 gcloud dataproc clusters create $CLUSTER \
@@ -14,9 +43,9 @@ gcloud dataproc clusters create $CLUSTER \
     --single-node
 
 # submit job to Cloud Dataproc cluster
-gcloud dataproc jobs submit pyspark word-count.py \
+gcloud dataproc jobs submit pyspark main.py \
     --cluster=${CLUSTER} \
-    -- gs://${BUCKET_NAME}/ gs://${BUCKET_NAME}/
+    -- gs://${BUCKET_NAME}/
 
 # delete the cloud Dataproc cluster
 gcloud dataproc clusters delete $CLUSTER
