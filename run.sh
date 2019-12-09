000345fa7b93fb42f83fe1c6240d5e25bbe6565d
aayush <aayushyadav96@gmail.com>
2019-12-08 19:16:25 -0500

    test-1

--- run.sh
+++ run.sh
@@ -3,48 +3,38 @@
 # set local environment variables
 PROJECT='book-recommendations-259715 '
 BUCKET_NAME='bookreview_bucket'
-CLUSTER='reccluster'
+CLUSTER='democluster'
 ZONE='us-west1-a'
 
-# create the working directories and chunk the data if not already chunked
-if ! [ -d gs://${BUCKET_NAME}/chunks/reviews ]; then
-    mkdir -p gs://${BUCKET_NAME}/chunks/reviews
-    split --bytes 500M --numeric-suffixes --suffix-length=3 \
-          --additional-suffix=.json gs://${BUCKET_NAME}/review_Books_5.json \
-          reviews_
-    mv gs://${BUCKET_NAME}/reviews_0*.json gs://${BUCKET_NAME}/chunks/reviews/
-    # rm review_Books_5.json
-fi
+# cluster configuration
+NUM_MASTERS=1
+MASTER_MACHINE_TYPE='n1-standard-2'
+NUM_WORKERS=3
+WORKER_MACHINE_TYPE='n1-standard-2'
 
-if ! [ -d gs://${BUCKET_NAME}/chunks/metabooks ]; then
-    mkdir -p gs://${BUCKET_NAME}/chunks/metabooks
-    split --bytes 500M --numeric-suffixes --suffix-length=3 \
-          --additional-suffix=.json gs://${BUCKET_NAME}/metaBooks.json \
-          metabooks_
-    mv gs://${BUCKET_NAME}/metabooks_0*.json gs://${BUCKET_NAME}/chunks/metabooks/
-    # rm metaBooks.json
-fi
-
-# chunk the full dataset if it has been generated
-if [ -f gs://${BUCKET_NAME}/dumps/processed.json ] && \
- ! [ -d gs://${BUCKET_NAME}/chunks/processed ]; then
-    mkdir -p gs://${BUCKET_NAME}/chunks/processed
-    split --bytes 500M --numeric-suffixes --suffix-length=4 \
-          --additional-suffix=.json gs://${BUCKET_NAME}/processed.json \
-          processed_
-    mv gs://${BUCKET_NAME}/processed_0*.json gs://${BUCKET_NAME}/chunks/processed/
-    # rm -r chunks/metabooks chunks/reviews
-fi
+# TO DO: write configuration yaml
 
 # create a cloud Dataproc cluster
 gcloud dataproc clusters create $CLUSTER \
     --project=${PROJECT} \
     --zone=${ZONE} \
-    --single-node
+    --image-version 1.3 \
+    --num-masters=${NUM_MASTERS} \
+    --master-machine-type=${MASTER_MACHINE_TYPE} \
+    --num-workers=${NUM_WORKERS} \
+    --worker-machine-type=${WORKER_MACHINE_TYPE} \
+    --metadata 'MINICONDA_VARIANT=3' \
+    --metadata 'MINICONDA_VERSION=latest' \
+    --initialization-actions \
+    gs://dataproc-initialization-actions/conda/bootstrap-conda.sh,gs://dataproc-initialization-actions/conda/install-conda-env.sh
+
+# zip python packages
+zip -r pypackage.zip scripts/ utils/ models/
 
 # submit job to Cloud Dataproc cluster
-gcloud dataproc jobs submit pyspark main.py \
+gcloud dataproc jobs submit pyspark test.py \
     --cluster=${CLUSTER} \
+    --py-files pypackage.zip \
     -- gs://${BUCKET_NAME}/
 
 # delete the cloud Dataproc cluster
--- scripts/chunker.sh
+++ scripts/chunker.sh
@@ -5,9 +5,9 @@ PROJECT='book-recommendations-259715 '
 BUCKET_NAME='bookreview_bucket'
 
 # chunk the full dataset if it has been generated
-mkdir -p gs://${BUCKET_NAME}/chunks/processed
+mkdir -p chunks/processed
 split --bytes 500M --numeric-suffixes --suffix-length=4 \
-        --additional-suffix=.json gs://${BUCKET_NAME}/processed.json \
+        --additional-suffix=.json processed.json \
         processed_
-mv gs://${BUCKET_NAME}/processed_0*.json gs://${BUCKET_NAME}/chunks/processed/
-    # rm -r chunks/metabooks chunks/reviews
+mv processed_0*.json chunks/processed/
+# rm -r chunks/metabooks chunks/reviews
